171c8a1f78ade9cb138cdc09c37102ba8c5a890b
bjy9507 <bjy9507@naver.com>
2022-08-12 01:15:58 +0900

    today algorithm baekjoon_14888 등 외 3개

---
+++ src/baekjoon/Main_14888.java
@@ -0,0 +1,118 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+* 시간 제한 : 2초 메모리 : 128MB
+* N : 숫자의 갯수 1 <= N <= 11
+* Ai : 숫자의 범위 1 <= Ai <= 100
+* */
+public class Main_14888 {
+
+    private static int n, max, min;
+    private static LinkedList<String> operand;
+    private static List<String> operators;
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream file = new FileInputStream("./study_algorithm/res/baekjoon/operator_insert_input.txt");
+        BufferedReader in = new BufferedReader(new InputStreamReader(file));
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        long start = System.nanoTime();
+
+        n = Integer.parseInt(in.readLine());            // 숫자의 갯수
+        // n개의 숫자 할당
+        operand = new LinkedList<>();
+        operators = new ArrayList<>();
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for(int i = 0; i < n; i++) {
+            operand.add(i,st.nextToken());
+        }
+        // 사용가능한 연산자 갯수 할당
+        st = new StringTokenizer(in.readLine());
+        getOperator("+",Integer.parseInt(st.nextToken()));
+        getOperator("-",Integer.parseInt(st.nextToken()));
+        getOperator("*",Integer.parseInt(st.nextToken()));
+        getOperator("/",Integer.parseInt(st.nextToken()));
+
+//        System.out.println(operators);
+//        System.out.println(operand);
+        String[] temp = new String[n-1];        // 연산식을 저장할 공간
+        expression(temp,0,0);
+
+        sb.append(max).append("\n").append(min);
+        System.out.println(sb);
+
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1000000 +"s");
+        in.close();
+
+    }
+
+    private static void expression(String[] temp,int cnt,int flag) {
+
+        if( cnt == n - 1 ) {
+//            System.out.println(Arrays.toString(temp));
+            String result;
+            Queue<String> q = new LinkedList<>();
+            for(int i = 0; i < n - 1;i++ ) {
+                q.offer(operand.get(i));
+                q.offer(temp[i]);
+            }
+            q.offer(operand.get(n-1));
+            result = q.poll();
+            while ( !q.isEmpty() ) {
+                String oper = q.poll();
+                int op2 = Integer.parseInt(q.poll());
+                result = getOperater(Integer.parseInt(result),op2,oper);
+            }
+            int tmp = Integer.parseInt(result);
+            if( max < tmp) max = tmp;
+            if( min > tmp) min = tmp;
+            return;
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+            if( (flag & 1<<i) != 0) continue;
+            temp[cnt] = operators.get(i);
+            /*System.out.println(temp);*/
+            expression(temp,cnt + 1,flag | 1<<i);
+        }
+    }
+
+    private static String getOperater(int op1 , int op2, String operator) {
+        switch ( operator ) {
+            case "+":
+                return String.valueOf(op1 + op2);
+            case "-":
+                return String.valueOf(op1 - op2);
+            case "*":
+                return String.valueOf(op1 * op2);
+            case "/":
+                int result = 0;
+                if( op1 < 0) {
+                    op1 = -op1;
+                    result = -(op1 / op2);
+                    return String.valueOf(result);
+                }
+                result = (op1 / op2);
+                return String.valueOf(result);
+            default:
+                return null;
+        }
+    }
+
+    private static void getOperator(String order, int count) {
+        for(int i = 1; i <= count; i++) {
+            operators.add(order);
+        }
+    }
+}
---
+++ src/baekjoon/Main_1991.java
@@ -0,0 +1,64 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_1991 {
+
+    static Map<String,char[]> nodes;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int height = Integer.parseInt(in.readLine());
+
+        nodes = new HashMap<>();
+        for(int i = 0; i < height; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            String key = st.nextToken();
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+            nodes.put(key,new char[]{left,right});
+        }
+
+        preOrder('A');
+        System.out.println();
+        inOrder('A');
+        System.out.println();
+        postOrder('A');
+    }
+
+    private static void preOrder(char node) {
+
+        char left  = nodes.get(String.valueOf(node))[0];
+        char right = nodes.get(String.valueOf(node))[1];
+
+        System.out.print(node);
+        if( left != '.' ) preOrder(left);
+        if( right != '.' ) preOrder(right);
+
+    }
+    private static void inOrder(char node) {
+
+        char left  = nodes.get(String.valueOf(node))[0];
+        char right = nodes.get(String.valueOf(node))[1];
+
+        if( left != '.' ) inOrder(left);
+        System.out.print(node);
+        if( right != '.' ) inOrder(right);
+
+    }
+    private static void postOrder(char node) {
+
+        char left  = nodes.get(String.valueOf(node))[0];
+        char right = nodes.get(String.valueOf(node))[1];
+
+        if( left != '.' ) postOrder(left);
+        if( right != '.' ) postOrder(right);
+        System.out.print(node);
+
+    }
+
+}
