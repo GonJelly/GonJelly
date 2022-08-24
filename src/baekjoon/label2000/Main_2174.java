cee5e96b0ee76498913f13103018acdd2c5b3a11
Gonjelly <bjy9507@naver.com>
2022-08-25 02:48:29 +0900

    kruskal algorithm and ãssafy daily algo

--- src/baekjoon/label2000/Main_2174.java
+++ src/baekjoon/label2000/Main_2174.java
@@ -1,40 +1,16 @@
-package baekjoon;
+package baekjoon.label2000;
+
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main_2174_tmp {
-
-    static class Robot {
-
-        int x;
-        int y;
-        int z;
-
-        public Robot(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if( this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Robot robot = (Robot) o;
-            return x == robot.x && y == robot.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-
+public class Main_2174 {
     private static int A, B;
     private static int[][] pos = {{1,0},{0,1},{-1,0},{0,-1}}; // 상좌하우
+    private static int[][] map;
 
     public static void main(String[] args) throws IOException {
 
@@ -47,21 +23,42 @@ public class Main_2174_tmp {
             A = Integer.parseInt(st.nextToken());   // 땅(지도)의 가로
             B = Integer.parseInt(st.nextToken());   // 땅(지도)의 세로
 
-            HashMap<Integer,Robot> robots = new HashMap<>();
+            map = new int[B + 1][A + 1];
+//        printMap();
 
             st = new StringTokenizer(in.readLine());
             int N = Integer.parseInt(st.nextToken());   // 로봇의 수 시간:1
             int M = Integer.parseInt(st.nextToken());   // 명령의 수 시간:1
 
+            List<int[]> robot = new ArrayList<>();
+            robot.add(new int[]{0, 0});
             for (int a = 1; a <= N; a++) {
                 st = new StringTokenizer(in.readLine());
                 int x = Integer.parseInt(st.nextToken());   // 로봇의 x축 위치
                 int y = Integer.parseInt(st.nextToken());   // 로봇의 y축 위치
                 int z = getPostion(st.nextToken());         // 가리키고 있는 방향
-                robots.put(a,new Robot(x,y,z));               // 로봇 위치 방향
+                map[y][x] = a;                              // 로봇 번호 기입
+                robot.add(new int[]{x, y, z});               // 시간:1
 //            System.out.println(Arrays.toString(robot.get(a)));
             }
 
+//        printMap();
+
+//            Collections.sort(robot, (o1, o2) -> {
+//                if (o1[1] == o2[1]) return (o1[0] - o2[0] > 0) ? 1 : -1;
+//                else if (o1[1] - o2[1] > 0) return 1;
+//                else return -1;
+//            });
+
+//        for(int a = 1; a <= N; a++) {
+//            System.out.println(Arrays.toString(robot.get(a)));
+//        }
+            // 입력한 순서대로 로봇 번호 기입
+//            for(int a = 1; a <= N; a++) {
+//                map[robot.get(a)[1]][robot.get(a)[0]] = a;
+//            }
+
+//            printMap();
             // 명령어 입력
             List<String[]> command = new ArrayList<>();
             for (int b = 0; b < M; b++) {
@@ -74,7 +71,7 @@ public class Main_2174_tmp {
 
             // 명령수행
             for (int a = 0; a < M; a++) {
-                valiable = getCommand(0, command.get(a), robots);
+                valiable = getCommand(0, command.get(a), robot);
                 if (!valiable) {
                     break;
                 }
@@ -95,57 +92,63 @@ public class Main_2174_tmp {
         }
     }
 
-    private static boolean getCommand(int cnt, String[] command, HashMap<Integer,Robot> robots) {
+    private static boolean getCommand(int cnt, String[] command , List<int[]> robot) {
 
         int robot_NM = Integer.parseInt(command[0]);
         if( cnt != 0 ) {
             // 벽에 붙이쳐서 명령 종료!!
-            if (robots.get(robot_NM).y <= 0 || robots.get(robot_NM).y > B ||
-                    robots.get(robot_NM).x <= 0 || robots.get(robot_NM).x > A) {
+            if (robot.get(robot_NM)[1] <= 0 || robot.get(robot_NM)[1] > B ||
+                    robot.get(robot_NM)[0] <= 0 || robot.get(robot_NM)[0] > A) {
                 System.out.println("Robot " + robot_NM + " crashes into the wall");
+//                printMap();
                 return false;
             }
-
             // 로봇과 충돌하여 명령 종료!!
-            int crash_NM = isCheck( robot_NM , robots ); // 충돌하지 않으면 -1
-            if( crash_NM != -1 ) {
-                System.out.println("Robot " + robot_NM + " crashes into robot " + crash_NM);
+            if (map[robot.get(robot_NM)[1]][robot.get(robot_NM)[0]] != 0) {
+                System.out.println("Robot " + robot_NM + " crashes into robot " + map[robot.get(robot_NM)[1]][robot.get(robot_NM)[0]]);
+//                printMap();
                 return false;
+            } else {    // 충돌하지 않는다면 로봇 이동
+                map[robot.get(robot_NM)[1]][robot.get(robot_NM)[0]] = robot_NM;
             }
 
         }
         // 명령을 모두 수행하면 종료
         if( cnt == Integer.parseInt(command[2]) ) {
+//            printMap();
             return true;
         }
 
+        map[robot.get(robot_NM)[1]][robot.get(robot_NM)[0]] = 0;
+
         switch ( command[1] ) {
             case "L":   // 왼쪽으로 90도 회전
-                robots.get(robot_NM).z += 3;
-                if( robots.get(robot_NM).z >= 4 ) robots.get(robot_NM).z %= 4;
-                return getCommand(cnt + 1, command, robots);
+                robot.get(robot_NM)[2] += 3;
+                if( robot.get(robot_NM)[2] >= 4) robot.get(robot_NM)[2] %= 4;
+//                System.out.println(Arrays.toString(robot.get(robot_NM)));
+                return getCommand(cnt + 1, command, robot);
             case "R":  // 오른쪽으로 90도 회전
-                robots.get(robot_NM).z += 1;
-                if( robots.get(robot_NM).z >= 4 ) robots.get(robot_NM).z %= 4;
-                return getCommand(cnt + 1, command, robots);
+                robot.get(robot_NM)[2] += 1;
+                if( robot.get(robot_NM)[2] >= 4) robot.get(robot_NM)[2] %= 4;
+//                System.out.println(Arrays.toString(robot.get(robot_NM)));
+                return getCommand(cnt + 1, command, robot);
             case "F":  // 가리키고 있는 방향 앞으로 한칸 전진
-                int way = robots.get(robot_NM).z;
-                robots.get(robot_NM).x += pos[way][1];
-                robots.get(robot_NM).y += pos[way][0];
-                return getCommand(cnt + 1, command, robots);
+                int way = robot.get(robot_NM)[2];
+                robot.get(robot_NM)[0] += pos[way][1];
+                robot.get(robot_NM)[1] += pos[way][0];
+//                System.out.println(Arrays.toString(robot.get(robot_NM)));
+                return getCommand(cnt + 1, command, robot);
         }
 
         return true;
     }
-    // 같은 로봇이 있는지 확인
-    private static int isCheck(int key, HashMap<Integer,Robot> robots) {
-        int n = robots.size();
-        Robot robot = robots.get(key);
-        for(int i = 1; i <= n; i++) {
-            if( key == i) continue;
-            if( robot.equals(robots.get(i)) ) return i;
-        }
-        return -1;
-    }
 
+    private static void printMap() {
+        for(int a = B; a >= 1; a--) {            // A 최대 100 B 최대 100 ==> 최대 10000번
+            for(int b = 1; b <= A; b++) {
+                System.out.print(map[a][b] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
