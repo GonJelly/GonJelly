4f2d8d54946f117cacf1939df0438cee4397ae73
Gonjelly <bjy9507@naver.com>
2022-08-27 15:20:29 +0900

    home 0827

--- src/swea/SW_4013.java
+++ src/swea/SW_4013.java
@@ -18,16 +18,19 @@ public class SW_4013 {
     public static void main(String[] args) throws IOException {
 
 //        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        FileInputStream file = new FileInputStream("./res/swea/cycle.txt");
+        FileInputStream file = new FileInputStream("./study_algorithm/res/swea/cycle1.txt");
         BufferedReader in = new BufferedReader(new InputStreamReader(file));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(in.readLine()); // 테스트 케이스 횟수
         // start
         for( int t = 1; t <= T; t++) {
 
-            int cnt = Integer.parseInt(in.readLine());
+            int cnt = Integer.parseInt(in.readLine());  // 톱니바퀴를 돌리는 경우의 수
             StringTokenizer st;
-            gears = new LinkedList[5];
+            int total = 0;                              // 최종 빨간색 화살표에 찍힌 점수 합산 저장
+            gears = new LinkedList[5];                  // 톱니바퀴의 바퀴정보 저장
+            // 톱니 바퀴 선언
             for(int i = 0; i <= 4; i++) {
                 gears[i] = new LinkedList<>();
             }
@@ -37,10 +40,10 @@ public class SW_4013 {
                 for(int c = 1; c <= 8; c++) {
                     gears[r].add(Integer.parseInt(st.nextToken()));
                 }
-                System.out.println(gears[r].size());
+//                System.out.println(gears[r]);
             }
-
-            turn = new int[5]; // 회전방향 제시 0: 0이면 노회전
+            // 톱니바퀴를 회전가능 여부 판단 0:정지 1:시계방향 -1:반시계방향
+            turn = new int[5]; // 회전방향 제시 0이면 노회전
 
             for(int i = 0; i < cnt; i++) {
                 st = new StringTokenizer(in.readLine());
@@ -48,17 +51,25 @@ public class SW_4013 {
                 int idx = Integer.parseInt(st.nextToken()); // 회전 시킬 톱니의 번호
                 int dir = Integer.parseInt(st.nextToken()); // 회전 시킬 톱니의 방향 및 횟수 ( 음수 : 반시계 양수 : 시계 );
 
-                System.out.printf("%d <--> %d",idx,dir);
+//                System.out.printf("%d <--> %d",idx,dir);
+                // 재귀를 돌면서 톱니를 돌린다.
                 cycle(0,idx,dir);
             }
 
+            for(int i = 1; i <= 4; i++)
+                if( gears[i].peek() == 1 ) {
+                    total += Math.pow(2,i - 1);
+                }
 
+            sb.append("#").append(t).append(" ").append(total).append("\n");
         }
 
-
+        System.out.println(sb);
     }
 
     private static void cycle(int count, int idx, int dir) {
+
+        if( count == Math.abs(dir) ) return;
 
         // 반시계방향
         if( dir < 0) {
@@ -112,22 +123,31 @@ public class SW_4013 {
             right++;
         }
 
-        System.out.println(Arrays.toString(turn));
+//        System.out.println(Arrays.toString(turn));
 
         for(int i = 1; i <= 4; i++) {
             if( turn[i] != 0) {
                 // 시계방향으로 회전
                 if( turn[i] == 1 ) {
-
+                    // 마지막을 첫번째로
+                    int rear = gears[i].pollLast();
+                    gears[i].addFirst(rear);
                 }
                 // 반시계방향으로 회전
                 else if( turn[i] == -1) {
+                    // 첫번째를 마지막으로
+                    int first = gears[i].pollFirst();
+                    gears[i].addLast(first);
                 }
             }
         }
+        // 확인용
+//        for(LinkedList<Integer> tmp : gears) {
+//            System.out.println(tmp);
+//        }
 
         Arrays.fill(turn,0);
 
-
+        cycle(count + 1, idx, dir);
     }
 }
